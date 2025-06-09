c3e03d111da9c7813985bc00158bc487474a3664
silgi <149964207+Silgimusicbotadmin@users.noreply.github.com>
2025-06-09 23:39:40 +0400

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORESETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~YaFJfkSqEohPFQzLfUZW",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~3M8f1owikxFfSLOEe3wN",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "SUBZERO-MD",  // Bot's display name
     MODE: getConfig("MODE") || process.env.MODE || "public",        // Bot mode: public/private/group/inbox
